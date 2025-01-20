df3941b7b7f1991b8a8aee2271ffb21b81dcf9c7
ohhyungsuh <ohhyungsuh@gmail.com>
2025-01-20 11:11:48 +0900

    feat: group 엔티티 생성
    - user_group 통해 다대다 풀었다
    - 단방향 매핑만 추가

---
+++ src/main/java/com/example/diary/group/domain/Group.java
@@ -0,0 +1,23 @@
+package com.example.diary.group.domain;
+
+import com.example.diary.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "groups")
+public class Group extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+}
